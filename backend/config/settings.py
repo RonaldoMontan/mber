7f574ea05b6423fbf315ed5379a2d1b99a58d98b
Bruno Lima <brunoluislima19@gmail.com>
2026-04-29 08:50:49 -0300

    fix: adiciona token_blacklist ao INSTALLED_APPS para suporte a logout com JWT

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -83,6 +83,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
     'accounts',
     'menu',
